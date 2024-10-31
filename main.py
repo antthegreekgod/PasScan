5d9fd01d76200d6a316fdce1a61b29867e81c4b4
ant <ant@localhost.localdomain>
2024-10-31 11:49:12 +0100

    Forgot to update changes

--- main.py
+++ main.py
@@ -3,20 +3,50 @@
 from crt import CrtPassiveScan
 import parse
 import threading
+import argparse
+import sys
+from pwn import *
 
+
+def get_options():
+
+    parser = argparse.ArgumentParser(prog="PasScan",
+                                     description="Passively scan a set of domains",
+                                     epilog="Author: @antthegreekgod"
+                                     )
+
+    parser.add_argument("-f", "--file", default="apex.lst", help="File containing apex domains, if no file is provided it will try to scan the current directory for a file named \"apex.lst\"")
+    parser.add_argument("-d", "--domain", default=None, help="Scan a single domain")
+    
+    return parser.parse_args()
 
 def query(domain):
+    p = log.progress("Scanning" + domain)
     CrtPassiveScan(domain).query() # make and save crt.sh petition
     subdomains = parse.ParseCrtSh("out.txt", domain).extract() # crawl file and extract subdomains
+    p.success("Scan completed!")
     print(subdomains)
 
 
 def main():
 
-    with open ("apex.lst", "r") as f:        
-        domains = f.read().split()
-    
+    args = get_options()
+
+    domains = []
+
+    if args.domain:
+        domains.append(args.domain)
+
+    try:    
+        with open (args.file, "r") as f:        
+            domains = f.read().split()
+    except FileNotFoundError:
+        pass
+
     threads = []
+    if not domains:
+        sys.exit("[!] No file nor domain was provided to scan. Use -h or --help to view the help menu")
+
     for domain in domains:
         thread = threading.Thread(target=query, args=(domain,))
         threads.append(thread)
--- parse.py
+++ parse.py
@@ -9,22 +9,50 @@ class ParseCrtSh:
         self.domain = domain
         self.pattern = "\S.*" + self.domain # pattern where string does not contain white space character
         self.clean = "<BR>|>|<|\*"
+
+    @staticmethod
+    def eliminate_duplicates(duplicates):
+
+        unique = list(dict.fromkeys(duplicates)) # eliminate duplicates
+
+        for elem in range(len(unique)): # get rid off intial annoying dot
+            if unique[elem][0] == '.':
+                unique[elem] = unique[elem][1:]
         
-    def extract(self):
-        
-        # initial cleanup
+        return unique
+    
+    def cleanup(self):
         with open(self.filename, "r") as f:
-            matches = re.sub(self.clean, '\n', f.read())
+            with open("temp.txt", "w") as w:
+                w.write(re.sub(self.clean, '\n', f.read()))
 
-        with open("temp.txt", "w") as w:
-            w.write(matches)
 
-        # extract matches
+    def find_match(self):
         with open("temp.txt", "r") as z:   
-            subdomains = re.findall(self.pattern, z.read())
-
-        # remove junk files
+            return re.findall(self.pattern, z.read())[9:] # first 9 positions in list are rubbish
+        
+    
+    @staticmethod
+    def delete_temp_files():
         os.remove("temp.txt")
         os.remove("out.txt")
+
+    def extract(self):
+
+        # make some modifications to file with crt.sh response for easier regex pattern matching
+        self.cleanup()    
+
+        # extract matches
+        subdomains_duplicates = self.find_match()
+        
+        # remove duplicates
+        subdomains = self.eliminate_duplicates(subdomains_duplicates)
+
+        # delete temp files
+        self.delete_temp_files()
+
         return subdomains
         
+
+if __name__ == "__main__":
+    ParseCrtSh("out.txt", "escriba.es").extract()
